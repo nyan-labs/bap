ab00cdb77acef3e57d50d617818ba46875de557a
qzip <me@qzip.gay>
2025-08-24 22:25:00 +0300

    the first ever commit yup uhuh

---
+++ src/routes/(app)/app/+page.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  import { m } from "$lib/paraglide/messages";
+</script>
---
+++ src/routes/(landing)/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { m } from "$lib/paraglide/messages";
+</script>
+
+welcome to site
+<a href="/app">go app</a>
